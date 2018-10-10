5b35b6a3745ad8be549c48cab61a16ca10593137
Matt Liberty <matt.liberty@jetperch.com>
2018-10-09 20:18:33 -0400

    Modified to only support Python 3.6+ due to "secrets" module dependency.

--- setup.py
+++ setup.py
@@ -59,9 +59,6 @@ setuptools.setup(
         'Topic :: Security :: Cryptography',
 
         # Supported Python versions
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: C',
